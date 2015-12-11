b42ff2d56ea5351b3559bbc5f78a88eb774d344d
daphshez <daphna@shezaf.com>
2015-12-11 01:48:23 +0000

    gtfs.py: fixed encoding issue; return everything, not only trups

--- gtfs.py
+++ gtfs.py
@@ -1,6 +1,7 @@
 __author__ = 'Daphna'
 
 from collections import namedtuple, defaultdict
+from io import TextIOWrapper
 from csv import DictReader
 from zipfile import ZipFile
 
@@ -12,18 +13,20 @@ Stop = namedtuple('Stop', 'stop_id stop_name')
 StopTime = namedtuple('StopTime', 'trip_id arrival_time departure_time stop stop_sequence')
 # stops is a list of StopTime
 Trip = namedtuple('Trip', 'route service trip_id trip_headsign direction_id stops')
+# all data
+Schedule = namedtuple('Schedule', 'routes services stops trips')
 
 
 def load(zip_name):
-    def calendar_get_day(row):
+    def calendar_get_day(r):
         days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-        values = [row[day] for day in days_of_week]
+        values = [r[day] for day in days_of_week]
         return values.index('1')
 
     def load_stop_times(reader):
         stop_times = defaultdict(lambda: [])
         for r in reader:
-            stop = stops[row['stop_id']]
+            stop = stops[r['stop_id']]
             # trip_id arrival_time departure_time stop stop_sequence
             stop_time = StopTime(r['trip_id'], r['arrival_time'], r['departure_time'], stop, r['stop_sequence'])
             stop_times[r['trip_id']].append(stop_time)
@@ -37,23 +40,25 @@ def load(zip_name):
 
     with ZipFile(zip_name) as z:
         with z.open('routes.txt') as routes_file:
-            reader = DictReader(routes_file)
+            reader = DictReader(TextIOWrapper(routes_file, 'utf-8-sig'))
             routes = {r['route_id']: Route(r['route_id'], r['route_long_name']) for r in reader}
 
         with z.open('calendar.txt') as services_file:
-            reader = DictReader(services_file)
+            reader = DictReader(TextIOWrapper(services_file, 'utf-8-sig'))
             services = {r['service_id']: Service(r['service_id'],
                                                  calendar_get_day(r),
                                                  r['start_date'], r['end_date']) for r in reader}
 
         with z.open('stops.txt') as stops_file:
-            reader = DictReader(stops_file)
+            reader = DictReader(TextIOWrapper(stops_file, 'utf-8-sig'))
             stops = {r['stop_id']: Stop(r['stop_id'], r['stop_name']) for r in reader}
 
         with z.open('stop_times.txt') as stop_times_file:
-            reader = DictReader(stop_times_file)
+            reader = DictReader(TextIOWrapper(stop_times_file, 'utf-8-sig'))
             stop_times = load_stop_times(reader)
 
         with z.open('trips.txt') as trips_file:
-            reader = DictReader(trips_file)
-            return [make_trip(r) for r in reader]
+            reader = DictReader(TextIOWrapper(trips_file, 'utf-8-sig'))
+            trips = [make_trip(r) for r in reader]
+
+        return Schedule(routes, services, stops, trips)
